3a81c7fe5fb038bd7860db5f938afb8e73664ef6
Sam MacPherson <xylom44@gmail.com>
2025-03-14 09:22:43 +0000

    update comments

--- src/proposals/20250320/SparkArbitrumOne_20250320.sol
+++ src/proposals/20250320/SparkArbitrumOne_20250320.sol
@@ -5,16 +5,16 @@ import { SparkPayloadArbitrumOne, Arbitrum } from "../../SparkPayloadArbitrumOne
 
 /**
  * @title  March 20, 2025 Spark Base Proposal
- * @notice Onboard Aave USDC, Onboard Fluid sUSDS
+ * @notice Onboard Aave USDC
  * @author Phoenix Labs
  * Forum:  https://forum.sky.money/t/march-20-2025-proposed-changes-to-spark-for-upcoming-spell/26113
  * Vote:   https://vote.makerdao.com/polling/Qmf4PDcJ
- *         https://vote.makerdao.com/polling/QmZbk3gD
  */
 contract SparkArbitrumOne_20250320 is SparkPayloadArbitrumOne {
 
     function execute() external {
         _onboardAaveToken(Arbitrum.ATOKEN_USDC, 30_000_000e6, 15_000_000e6 / uint256(1 days));
+        // Note: Fluid sUSDS is being pushed because it's not available
     }
 
 }
