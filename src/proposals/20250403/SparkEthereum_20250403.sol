3394d03d65e13460b952b1dfcef4582e78452491
Sam MacPherson <xylom44@gmail.com>
2025-03-25 07:29:38 -0400

    adding controller upgrade

--- src/proposals/20250403/SparkEthereum_20250403.sol
+++ src/proposals/20250403/SparkEthereum_20250403.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.25;
 
+import { Arbitrum } from "spark-address-registry/Arbitrum.sol";
+import { Base }     from "spark-address-registry/Base.sol";
 import { Ethereum } from 'spark-address-registry/Ethereum.sol';
+
+import { CCTPForwarder } from "xchain-helpers/forwarders/CCTPForwarder.sol";
+
+import { ControllerInstance }              from "spark-alm-controller/deploy/ControllerInstance.sol";
+import { MainnetControllerInit }           from "spark-alm-controller/deploy/MainnetControllerInit.sol";
+import { MainnetController }               from "spark-alm-controller/src/MainnetController.sol";
+import { RateLimitHelpers, RateLimitData } from "spark-alm-controller/src/RateLimitHelpers.sol";
 
 import { SparkPayloadEthereum } from "../../SparkPayloadEthereum.sol";
 
@@ -20,6 +29,42 @@ contract SparkEthereum_20250403 is SparkPayloadEthereum {
     }
 
     function _postExecute() internal override {
+        _upgradeController();
+    }
+
+    function _upgradeController() private {
+        MainnetControllerInit.MintRecipient[] memory mintRecipients = new MainnetControllerInit.MintRecipient[](2);
+        mintRecipients[0] = MainnetControllerInit.MintRecipient({
+            domain        : CCTPForwarder.DOMAIN_ID_CIRCLE_BASE,
+            mintRecipient : bytes32(uint256(uint160(Base.ALM_PROXY)))
+        });
+        mintRecipients[1] = MainnetControllerInit.MintRecipient({
+            domain        : CCTPForwarder.DOMAIN_ID_CIRCLE_ARBITRUM_ONE,
+            mintRecipient : bytes32(uint256(uint160(Arbitrum.ALM_PROXY)))
+        });
+
+        MainnetControllerInit.upgradeController({
+            controllerInst: ControllerInstance({
+                almProxy   : Ethereum.ALM_PROXY,
+                controller : NEW_ALM_CONTROLLER,
+                rateLimits : Ethereum.ALM_RATE_LIMITS
+            }),
+            configAddresses: MainnetControllerInit.ConfigAddressParams({
+                freezer       : Ethereum.ALM_FREEZER,
+                relayer       : Ethereum.ALM_RELAYER,
+                oldController : Ethereum.ALM_CONTROLLER
+            }),
+            checkAddresses: MainnetControllerInit.CheckAddressParams({
+                admin      : Ethereum.SPARK_PROXY,
+                proxy      : Ethereum.ALM_PROXY,
+                rateLimits : Ethereum.ALM_RATE_LIMITS,
+                vault      : Ethereum.ALLOCATOR_VAULT,
+                psm        : Ethereum.PSM,
+                daiUsds    : Ethereum.DAI_USDS,
+                cctp       : Ethereum.CCTP_TOKEN_MESSENGER
+            }),
+            mintRecipients: mintRecipients
+        });
     }
 
 }
