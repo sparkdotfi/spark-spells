93fa88cc535aaa40ebcd2dbac260443422c49e03
Tabish Shaikh <bonjour.tabish@gmail.com>
2025-06-05 18:15:41 +0530

    feat: spark base proposal

--- src/proposals/20250612/SparkBase_20250612.sol
+++ src/proposals/20250612/SparkBase_20250612.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.25;
 
-import { SparkPayloadBase, Base } from "../../SparkPayloadBase.sol";
+import { IMetaMorpho } from 'metamorpho/interfaces/IMetaMorpho.sol';
 
-interface IMorpho {
-    function setFee(uint256 newFee) external;
-    function setFeeRecipient(address newFeeRecipient) external;
-}
+import { SparkPayloadBase, Base } from "../../SparkPayloadBase.sol";
 
 /**
  * @title  June 12, 2025 Spark Base Proposal
- * @notice 
+ * @notice Morpho USDC Vault:
+ *         - Set fee and fee recipient
  * @author Phoenix Labs
- * Forum:  https://forum.sky.money/t/may-29-2025-proposed-changes-to-spark-for-upcoming-spell/26372
- * Vote:   https://vote.makerdao.com/polling/QmfPc8Ub
+ * Forum:  https://forum.sky.money/t/june-12-2025-proposed-changes-to-spark-for-upcoming-spell/26559
+ * Vote:   https://vote.sky.money/polling/QmdyVQok
  */
 contract SparkBase_20250612 is SparkPayloadBase {
 
     uint256 internal constant MORPHO_SPARK_USDC_VAULT_FEE = 0.1e18;
 
     function execute() external {
-        IMorpho(Base.MORPHO_VAULT_SUSDC).setFeeRecipient(Base.SPARK_EXECUTOR);
-        IMorpho(Base.MORPHO_VAULT_SUSDC).setFee(MORPHO_SPARK_USDC_VAULT_FEE);
+        IMetaMorpho(Base.MORPHO_VAULT_SUSDC).setFeeRecipient(Base.ALM_PROXY);
+        IMetaMorpho(Base.MORPHO_VAULT_SUSDC).setFee(MORPHO_SPARK_USDC_VAULT_FEE);
     }
 
 }
