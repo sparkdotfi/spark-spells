50ed43806f747a5811798829a4acf5f726e9e476
Michael De Luca <michael.deluca@circle-free.com>
2025-10-30 13:42:05 -0400

    fix: PR review

--- src/libraries/ChainIdUtils.sol
+++ src/libraries/ChainIdUtils.sol
@@ -7,12 +7,12 @@ import { Domain } from "xchain-helpers/testing/Domain.sol";
 library ChainIdUtils {
 
     function toDomainString(uint256 id) internal pure returns (string memory domainString) {
-        if (id == 1) return "Ethereum";
-        if (id == 100) return "Gnosis";
-        if (id == 8453) return "Base";
+        if (id == 1)     return "Ethereum";
+        if (id == 100)   return "Gnosis";
+        if (id == 8453)  return "Base";
         if (id == 42161) return "ArbitrumOne";
-        if (id == 10) return "Optimism";
-        if (id == 130) return "Unichain";
+        if (id == 10)    return "Optimism";
+        if (id == 130)   return "Unichain";
         if (id == 43114) return "Avalanche";
 
         require(false, "ChainIdUtils/invalid-chain-id");
