39ffdcec68f29bbdc77da4e25795851eed62e208
Tabish Shaikh <bonjour.tabish@gmail.com>
2026-04-16 10:15:30 +0530

    add: tests

--- src/proposals/20260423/Spell_20260423.t.sol
+++ src/proposals/20260423/Spell_20260423.t.sol
@@ -1,19 +1,35 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.25;
 
-import { IERC20 }         from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { IERC20Metadata } from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC20, SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20Metadata }    from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 import { Ethereum }  from "spark-address-registry/Ethereum.sol";
 import { SparkLend } from "spark-address-registry/SparkLend.sol";
 
-import { IPool } from "sparklend-v1-core/interfaces/IPool.sol";
+import { ReserveConfiguration } from "sparklend-v1-core/protocol/libraries/configuration/ReserveConfiguration.sol";
+import { UserConfiguration }    from "sparklend-v1-core/protocol/libraries/configuration/UserConfiguration.sol";
+
+import { AaveOracle }        from "sparklend-v1-core/misc/AaveOracle.sol";
+import { DataTypes }         from "sparklend-v1-core/protocol/libraries/types/DataTypes.sol";
+import { IPool }             from "sparklend-v1-core/interfaces/IPool.sol";
+import { IPoolConfigurator } from "sparklend-v1-core/interfaces/IPoolConfigurator.sol";
 
 import { ChainIdUtils } from "src/libraries/ChainIdUtils.sol";
 
 import { SparklendTests }           from "src/test-harness/SparklendTests.sol";
 import { SparkLiquidityLayerTests } from "src/test-harness/SparkLiquidityLayerTests.sol";
 import { SpellTests }               from "src/test-harness/SpellTests.sol";
+
+contract MockAggregator {
+
+    int256 public latestAnswer;
+
+    constructor(int256 _latestAnswer) {
+        latestAnswer = _latestAnswer;
+    }
+
+}
 
 contract SparkEthereum_20260423_SLLTests is SparkLiquidityLayerTests {
 
@@ -31,6 +47,10 @@ contract SparkEthereum_20260423_SLLTests is SparkLiquidityLayerTests {
 }
 
 contract SparkEthereum_20260423_SparklendTests is SparklendTests {
+
+    using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
+    using UserConfiguration for DataTypes.UserConfigurationMap;
+    using SafeERC20 for IERC20;
 
     address internal constant OLD_USDT_IRM = 0x2961d766D71F33F6C5e6Ca8bA7d0Ca08E6452C92;
     address internal constant NEW_USDT_IRM = 0x4E494988E68e6Fc52309BE4937869e27F0C304AC;
@@ -105,32 +125,148 @@ contract SparkEthereum_20260423_SparklendTests is SparklendTests {
         rethConfigAfter.isFrozen = true;
 
         _validateReserveConfig(rethConfigAfter, allConfigsAfter);
+
+        _testUserActionsOnFrozenReserve(Ethereum.RETH, Ethereum.USDC);
     }
 
-    function test_ETHEREUM_sparkLend_deprecateRETH_e2e() external onChain(ChainIdUtils.Ethereum()) {
-        _executeAllPayloadsAndBridges();
-
+    function _testUserActionsOnFrozenReserve(address reserveAsset, address debtAsset) internal {
         address testUser      = makeAddr("testUser");
-        uint256 reserveAmount = 100 * 10 ** IERC20Metadata(Ethereum.RETH).decimals();
-        uint256 debtAmount    = 10 * 10 ** IERC20Metadata(Ethereum.USDC).decimals();
+        uint256 reserveAmount = 100 * 10 ** IERC20Metadata(reserveAsset).decimals();
+        uint256 debtAmount    = 10 * 10 ** IERC20Metadata(debtAsset).decimals();
 
         IPool pool = IPool(SparkLend.POOL);
 
-        // Check reserve freeze conditions (can't supply/borrow).
+        // --- Step 1: Check reserve freeze conditions (can't supply/borrow, can withdraw/repay)
 
-        deal(Ethereum.RETH, testUser, reserveAmount);
+        deal(reserveAsset, testUser, reserveAmount);
 
         vm.startPrank(testUser);
 
-        IERC20(Ethereum.RETH).approve(address(pool), type(uint256).max);
+        IERC20(reserveAsset).approve(address(pool), type(uint256).max);
 
         // User can't supply.
         vm.expectRevert(bytes("28"));  // RESERVE_FROZEN
-        pool.supply(Ethereum.RETH, reserveAmount, testUser, 0);
+        pool.supply(reserveAsset, reserveAmount, testUser, 0);
 
         // User can't borrow.
         vm.expectRevert(bytes("28"));  // RESERVE_FROZEN
-        pool.borrow(Ethereum.RETH, debtAmount, 2, 0, testUser);
+        pool.borrow(reserveAsset, debtAmount, 2, 0, testUser);
+
+        // User can repay when conditions are correct.
+        vm.expectRevert(bytes("39"));  // NO_DEBT_OF_SELECTED_TYPE (past RESERVE_FROZEN error, able to repay if there is debt)
+        pool.repay(reserveAsset, debtAmount, 2, testUser);
+
+        // User can withdraw collateral when conditions are correct.
+        vm.expectRevert(bytes("32"));  // NOT_ENOUGH_AVAILABLE_USER_BALANCE (past RESERVE_FROZEN error, able to withdraw if there is collateral)
+        pool.withdraw(reserveAsset, reserveAmount, testUser);
+
+        vm.stopPrank();
+
+        // --- Step 2: Check collateral behaviour when borrowing another borrowable asset
+        //             (should be able to withdraw collateral, repay borrowAsset, and get liquidated)
+
+        // If the reserve is not active or has a debt ceiling, skip the test as user collateral enabled will be false.
+        if (pool.getReserveData(reserveAsset).configuration.getLtv() == 0) return;
+        if (pool.getReserveData(reserveAsset).configuration.getDebtCeiling() > 0) return;
+
+        // Increase the supply cap and set up a new collateral position
+
+        IPoolConfigurator poolConfigurator = IPoolConfigurator(SparkLend.POOL_CONFIGURATOR);
+
+        uint256 currentSupplyCap = pool.getConfiguration(reserveAsset).getSupplyCap();
+
+        vm.prank(Ethereum.SPARK_PROXY);
+        poolConfigurator.setSupplyCap(reserveAsset, currentSupplyCap + 1_000_000);
+
+        _setupUserSparkLendPosition(reserveAsset, debtAsset, testUser, reserveAmount, debtAmount);
+
+        // User can repay the debt in the borrowAsset
+
+        deal(debtAsset, testUser, debtAmount);
+
+        vm.startPrank(testUser);
+
+        IERC20(debtAsset).safeIncreaseAllowance(address(pool), type(uint256).max);
+
+        pool.repay(debtAsset, debtAmount, 2, testUser);
+
+        vm.stopPrank();
+
+        // User can repay the debt in reserveAsset
+
+        if (pool.getConfiguration(reserveAsset).getBorrowingEnabled()) {
+            _setupUserSparkLendPosition(reserveAsset, reserveAsset, testUser, reserveAmount, debtAmount);
+
+            deal(reserveAsset, testUser, debtAmount);
+
+            vm.startPrank(testUser);
+
+            IERC20(reserveAsset).safeIncreaseAllowance(address(pool), type(uint256).max);
+
+            pool.repay(reserveAsset, debtAmount, 2, testUser);
+
+            vm.stopPrank();
+        }
+
+        // User can withdraw the collateral
+
+        vm.startPrank(testUser);
+
+        pool.withdraw(reserveAsset, 1 * 10 ** IERC20Metadata(reserveAsset).decimals(), testUser);
+
+        vm.stopPrank();
+
+        // User can get liquidated
+
+        _setupUserSparkLendPosition(reserveAsset, debtAsset, testUser, reserveAmount, debtAmount);
+
+        address mockOracle = address(new MockAggregator(1));
+
+        address[] memory assets  = new address[](1);
+        address[] memory sources = new address[](1);
+
+        assets[0]  = reserveAsset;
+        sources[0] = mockOracle;
+
+        vm.prank(Ethereum.SPARK_PROXY);
+        AaveOracle(SparkLend.AAVE_ORACLE).setAssetSources(assets, sources);
+
+        deal(debtAsset,    testUser, debtAmount);
+        deal(reserveAsset, testUser, reserveAmount);
+
+        // User can be liquidated.
+        vm.prank(testUser);
+        pool.liquidationCall(reserveAsset, debtAsset, testUser, debtAmount, false);
+    }
+
+    function _setupUserSparkLendPosition(
+        address collateralAsset,
+        address debtAsset,
+        address testUser,
+        uint256 collateralAmount,
+        uint256 debtAmount
+    ) internal {
+        IPool pool = IPool(SparkLend.POOL);
+
+        deal(collateralAsset, testUser, collateralAmount);
+
+        vm.prank(testUser);
+        IERC20(collateralAsset).approve(address(pool), type(uint256).max);
+
+        // Set Reserve frozen to false.
+        vm.prank(Ethereum.SPARK_PROXY);
+        IPoolConfigurator(SparkLend.POOL_CONFIGURATOR).setReserveFreeze(collateralAsset, false);
+
+        vm.startPrank(testUser);
+
+        pool.supply(collateralAsset, collateralAmount, testUser, 0);
+        pool.borrow(debtAsset,       debtAmount,       2,          0, testUser);
+
+        vm.stopPrank();
+
+        // Set Reserve frozen to true.
+        vm.prank(Ethereum.SPARK_PROXY);
+        IPoolConfigurator(SparkLend.POOL_CONFIGURATOR).setReserveFreeze(collateralAsset, true);
     }
 
 }
