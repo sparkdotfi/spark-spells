5381945f0a43a1fd58de1c6b7505c3b83a6847db
TheMj0ln1r <charan.nomula.cn@gmail.com>
2026-06-12 22:08:04 +0530

    fix: failed tests

--- src/test-harness/SpellRunner.sol
+++ src/test-harness/SpellRunner.sol
@@ -320,6 +320,7 @@ abstract contract SpellRunner is Test {
             uint256 chainId = chainData[allChains[i]].domain.chain.chainId;
 
             if (chainId == ChainIdUtils.Ethereum()) continue;  // Don't execute mainnet
+            if (chainData[chainId].payload == address(0)) continue; // test explicitly disabled this chain
 
             address   mainnetSpellPayload = _getForeignPayloadFromMainnetSpell(chainId);
             IExecutor executor            = chainData[chainId].executor;
