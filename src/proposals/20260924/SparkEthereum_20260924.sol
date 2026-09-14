52c19a99c5804ac82c67c1d158e2e2247846a0d6
Tabish Shaikh <bonjour.tabish@gmail.com>
2026-09-14 13:30:03 +0530

    initial commit

---
+++ src/proposals/20260924/SparkEthereum_20260924.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.25;
+
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import { Ethereum }  from "spark-address-registry/Ethereum.sol";
+
+import { SparkPayloadEthereum } from "../../SparkPayloadEthereum.sol";
+
+/**
+ * @title  September 24, 2026 Spark Ethereum Proposal
+ * @author Phoenix Labs
+ * @notice Spark Treasury:
+ *         - Transfer the October 2026 monthly grants to the Spark Foundation and the Spark Assets Foundation.
+ *         - Transfer USDS to the buyback executor to fund SPK buybacks.
+ * Forum:
+ */
+contract SparkEthereum_20260924 is SparkPayloadEthereum {
+
+    uint256 internal constant SPARK_FOUNDATION_GRANT_AMOUNT       = 865_000e18;
+    uint256 internal constant SPARK_ASSET_FOUNDATION_GRANT_AMOUNT = 45_000e18;
+
+    uint256 internal constant USDS_SPK_BUYBACK_AMOUNT = 972_485e18;  // TODO: change this amount acc. to proposal passes/not.
+
+    function _postExecute() internal override {
+        // 1. Transfer the October 2026 monthly grants to the Spark Foundation and the Spark Assets Foundation.
+        IERC20(Ethereum.USDS).transfer(Ethereum.SPARK_FOUNDATION_MULTISIG,       SPARK_FOUNDATION_GRANT_AMOUNT);
+        IERC20(Ethereum.USDS).transfer(Ethereum.SPARK_ASSET_FOUNDATION_MULTISIG, SPARK_ASSET_FOUNDATION_GRANT_AMOUNT);
+
+        // 2. Transfer USDS to the buyback executor to fund SPK buybacks.
+        IERC20(Ethereum.USDS).transfer(Ethereum.ALM_OPS_MULTISIG, USDS_SPK_BUYBACK_AMOUNT);
+    }
+
+}
---
+++ src/proposals/20260924/Spell_20260924.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.25;
+
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { Ethereum }  from "spark-address-registry/Ethereum.sol";
+
+import { ChainIdUtils } from "src/libraries/ChainIdUtils.sol";
+
+import { SparklendTests }           from "src/test-harness/SparklendTests.sol";
+import { SparkLiquidityLayerTests } from "src/test-harness/SparkLiquidityLayerTests.sol";
+import { SpellTests }               from "src/test-harness/SpellTests.sol";
+
+contract SparkEthereum_20260924_SLLTests is SparkLiquidityLayerTests {
+
+    uint256 internal constant USDG_BALANCES_SLOT_INDEX = 1;
+
+    constructor() {
+        _spellId   = 20260924;
+        _blockDate = 1789370917;  // 2026-09-14 07:28:37 UTC
+    }
+
+    function setUp() public override {
+        super.setUp();
+
+        // chainData[ChainIdUtils.Ethereum()].payload = 0xbE35b15Cda9002C1719A9D254B158613BDdE72af;
+    }
+
+    function deal(address token, address to, uint256 amount) internal override {
+        if (token == Ethereum.USDG) {
+            vm.store(Ethereum.USDG, keccak256(abi.encode(to, USDG_BALANCES_SLOT_INDEX)), bytes32(amount));
+            return;
+        }
+        super.deal(token, to, amount);
+    }
+
+}
+
+contract SparkEthereum_20260924_SparklendTests is SparklendTests {
+
+    constructor() {
+        _spellId   = 20260924;
+        _blockDate = 1789370917;  // 2026-09-14 07:28:37 UTC
+    }
+
+    function setUp() public override {
+        super.setUp();
+
+        // chainData[ChainIdUtils.Ethereum()].payload = 0xbE35b15Cda9002C1719A9D254B158613BDdE72af;
+    }
+
+}
+
+contract SparkEthereum_20260924_SpellTests is SpellTests {
+
+    uint256 internal constant SPARK_FOUNDATION_GRANT_AMOUNT       = 865_000e18;
+    uint256 internal constant SPARK_ASSET_FOUNDATION_GRANT_AMOUNT = 45_000e18;
+
+    uint256 internal constant USDS_SPK_BUYBACK_AMOUNT = 972_485e18;  // TODO: change this amount acc. to proposal passes/not.
+
+    constructor() {
+        _spellId   = 20260924;
+        _blockDate = 1789370917;  // 2026-09-14 07:28:37 UTC
+    }
+
+    function setUp() public override {
+        super.setUp();
+
+        // chainData[ChainIdUtils.Ethereum()].payload = 0xbE35b15Cda9002C1719A9D254B158613BDdE72af;
+    }
+
+    function test_ETHEREUM_sparkTreasury() external onChain(ChainIdUtils.Ethereum()) {
+        IERC20 usds = IERC20(Ethereum.USDS);
+
+        uint256 sparkProxyBalanceBefore      = usds.balanceOf(Ethereum.SPARK_PROXY);
+        uint256 foundationBalanceBefore      = usds.balanceOf(Ethereum.SPARK_FOUNDATION_MULTISIG);
+        uint256 assetFoundationBalanceBefore = usds.balanceOf(Ethereum.SPARK_ASSET_FOUNDATION_MULTISIG);
+        uint256 almOpsBalanceBefore          = usds.balanceOf(Ethereum.ALM_OPS_MULTISIG);
+
+        assertEq(sparkProxyBalanceBefore,      47_824_927.085806286854722044e18);
+        assertEq(foundationBalanceBefore,      2_733_790.0222e18);
+        assertEq(assetFoundationBalanceBefore, 155_000e18);
+        assertEq(almOpsBalanceBefore,          0);
+
+        _executeAllPayloadsAndBridges();
+
+        assertEq(usds.balanceOf(Ethereum.SPARK_PROXY),                     sparkProxyBalanceBefore - SPARK_FOUNDATION_GRANT_AMOUNT - SPARK_ASSET_FOUNDATION_GRANT_AMOUNT - USDS_SPK_BUYBACK_AMOUNT);
+        assertEq(usds.balanceOf(Ethereum.SPARK_FOUNDATION_MULTISIG),       foundationBalanceBefore + SPARK_FOUNDATION_GRANT_AMOUNT);
+        assertEq(usds.balanceOf(Ethereum.SPARK_ASSET_FOUNDATION_MULTISIG), assetFoundationBalanceBefore + SPARK_ASSET_FOUNDATION_GRANT_AMOUNT);
+        assertEq(usds.balanceOf(Ethereum.ALM_OPS_MULTISIG),                almOpsBalanceBefore + USDS_SPK_BUYBACK_AMOUNT);
+    }
+
+}
